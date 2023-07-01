eb894d60bff6b0c0188684aec5ffcaa22e716fd1
Davi Lemes <ndlemes7@outlook.com>
2023-07-01 14:26:33 -0300

    greedy refatorado

--- greedy/greedy.py
+++ greedy/greedy.py
@@ -11,7 +11,7 @@ def greedy_algorithm(sudoku):
         min_violations = float('inf')
         best_move = None
 
-        # Percorrer todas as posições vazias no Sudoku
+        # passa por todas as celulas vazias
         for i in range(9):
             for j in range(9):
                 if sudoku[i][j] == 0:
@@ -21,9 +21,9 @@ def greedy_algorithm(sudoku):
                         if new_violations < min_violations:
                             min_violations = new_violations
                             best_move = (i, j, num)
-                    sudoku[i][j] = 0  # Desfazer a tentativa
+                    sudoku[i][j] = 0  # desfaz tentativa de troca
 
-        # Atualizar o Sudoku com o movimento que reduziu as violações
+        # bota o numero com menos viloacoes
         if best_move is not None:
             i, j, num = best_move
             sudoku[i][j] = num
@@ -33,8 +33,17 @@ def greedy_algorithm(sudoku):
 
     return sudoku, violations
 
+sudukinho = [[0, 6, 0, 9, 0, 0, 3, 7, 2],
+[3, 9, 8, 7, 0, 6, 4, 1, 5],
+[2, 0, 7, 1, 3, 0, 0, 8, 9],
+[0, 0, 6, 0, 1, 0, 0, 3, 8],
+[0, 0, 0, 2, 5, 3, 0, 0, 1],
+[0, 0, 0, 8, 6, 7, 0, 9, 4],
+[0, 0, 3, 5, 9, 2, 1, 0, 6],
+[0, 0, 9, 6, 0, 1, 8, 0, 0],
+[6, 0, 2, 3, 0, 0, 0, 5, 7]]
 
-result, num_violations = greedy_algorithm(sudoku.playable_sudoku)
+result, num_violations = greedy_algorithm(sudukinho)
 
 print()
 print()
