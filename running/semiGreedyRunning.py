f6048c4985bf7be0ba88c45197e32589f1544841
Karolayne Mendes <karolaynesmendes.07@gmail.com>
2023-07-06 03:03:15 -0300

    Create semiGreedyRunning.py

---
+++ running/semiGreedyRunning.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('../sudoku')
+import utils
+from semigreedy.semigreedy_random import semi_greedy_algorithm
+
+for i in range(1, 11):
+    sudoku = utils.read_instance(i)
+    violations = 0
+
+    for j in range(10):
+        new_violations, new_sudoku = semi_greedy_algorithm(sudoku, 15)
+        violations += new_violations
+
+    violations = violations/10
+    print('Instância nº ' + i + ' - Média de violações: ' + violations)
